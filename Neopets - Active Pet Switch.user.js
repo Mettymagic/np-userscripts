7e59daa64b7128ba088e97d99428f7a847c5473b
TrueThaumaturge <quentin.jennings@shaw.ca>
2023-08-15 20:43:01 -0600

    [APS] Overhauled UI and now automatically grabs the pet data and auto-updates when needed

--- Neopets - Active Pet Switch.user.js
+++ Neopets - Active Pet Switch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets - Active Pet Switch <MettyNeo>
-// @version      1.0
+// @version      1.1
 // @description  Adds a button to the sidebar that lets you easily switch your active pet.
 // @author       Metamagic
 // @match        *://*.neopets.com/*
@@ -8,10 +8,13 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
+// @grant        GM_addValueChangeListener
 // ==/UserScript==
 
 // Trans rights are human rights ^^
 // metty says hi
+
+const HOME_DATA_TIMEOUT = 120 //collected page data times out after this many minutes
 
 //==============
 // main function
@@ -23,7 +26,7 @@ if($("[class^='nav-pet-menu-icon']").length) isBeta = true
 
 
 if(url.includes("neopets.com/home")) {
-    updatePetInfo()
+    getHomeData(document)
 }
 
 createMenuButton()
@@ -32,26 +35,30 @@ createMenuButton()
 // overhead
 //=========
 
-function updatePetInfo() {
-    let ref = getUserRef()
-    if(ref) GM_setValue("userref", ref)
-    let pets = getPets()
-    if(pets) GM_setValue("petlist", pets)
-    GM_setValue("username", getUsername())
-    console.log("[APS] Pet data recorded.")
+function requestHomeData() {
+    GM_setValue("waitfordata", true)
+    $.get("https://www.neopets.com/home/", function(data, status){
+        let doc = new DOMParser().parseFromString(data, "text/html")
+        getHomeData(doc)
+        console.log("[APS] Data successfully retrieved.")
+        GM_setValue("waitfordata", false)
+    })
 }
 
-function validateUser() {
-    if(getUsername() != GM_getValue("username")) {
-        console.log("[APS] New username detected, resetting pet data.")
-        GM_deleteValue("userref")
-        GM_deleteValue("petlist")
-        GM_deleteValue("username")
-        return false
-    }
-    return true
-}
+function getHomeData(doc) {
+    let oldRef = GM_getValue("userref")
+    let ref = getUserRef(doc)
+    if(ref && oldRef != ref) GM_setValue("userref", ref)
 
+    let oldPets = GM_getValue("petlist")
+    let pets = getPets(doc)
+    if(pets && oldPets != pets) GM_setValue("petlist", pets)
+
+    GM_setValue("lastupdate", new Date().valueOf())
+    GM_setValue("un", getUsername())
+
+    console.log("[APS] Home Page data updated.")
+}
 
 //=================
 // element creation
@@ -64,49 +71,52 @@ function createMenuButton() {
         let link = document.createElement("a")
         link.href = "#"
         link.classList.add("openmenubutton")
-        if(!isBeta) {
-            link.style.width = "146px"
-            link.innerHTML = "Change Active Pet"
-        }
-        else {
-            link.innerHTML = "Change Active Pet"
-        }
+        link.innerHTML = "Change Active Pet"
         link.addEventListener("click", ()=>{openMenu()})
         e.appendChild(link)
-        createMenu()
+        createMenu() //only adds menu to pages with buttons
     }
 }
 
 function createMenu() {
-    if(validateUser()) {
-        let menu = document.createElement("div")
-        menu.id = "select-active-pet-menu"
-        menu.classList.add("activepetmenu")
-        menu.style.display = "none"
-        if(url.includes("neopets.com/home")) menu.style.paddingTop = "65px"
+    let menu = document.createElement("div")
+    menu.id = "select-active-pet-menu"
+    menu.style.display = "none" //starts not visible
 
-        let header = document.createElement("div")
-        header.classList.add("activetableheader")
-        header.style.backgroundColor = getThemeColor()
-        header.style.color = getTextColor()
-        header.innerHTML = `<b>Select an Active Pet:</b><br><small><small><small>(Click anywhere else to exit)</small></small></small>`
+    let header = document.createElement("div")
+    header.classList.add("activetableheader")
+    header.innerHTML = `<b>Select an Active Pet:</b><br><small><small><small>(Click anywhere else to exit)</small></small></small>`
+    menu.appendChild(header)
 
-        menu.appendChild(header)
-        menu.appendChild(createTable())
-        if(isBeta) document.body.insertBefore(menu,$(".hp-bg-grid__2020")[0])
-        else $("#main")[0].insertBefore(menu, $("#content")[0])
+    //table
+    let table = document.createElement("table")
+    table.classList.add("activetable")
+    menu.appendChild(table)
+    document.body.appendChild(menu)
+
+    //revalidates data if it times out or if username changes
+    if(new Date().valueOf() - GM_getValue("lastupdate", 0) > 1000*60*HOME_DATA_TIMEOUT || getUsername() != GM_getValue("un") || !GM_getValue("petlist", false)) {
+        console.log("[APS] Data out of date or invalid, requesting new data.")
+        let load = document.createElement("div")
+        load.innerHTML = "( Fetching pet data ... )"
+        menu.appendChild(load)
+        GM_addValueChangeListener("waitfordata", function(key,oldValue,newValue,remote) {
+            populateTable()
+            menu.removeChild(load)
+        })
+        requestHomeData()
     }
+    else populateTable()
 }
 
-function createTable() {
-    let petList = Object.values(GM_getValue("petlist"))
-    let table = document.createElement("table")
+function populateTable() {
+    let petList = Object.values(GM_getValue("petlist", {}))
+    let table = $(".activetable")[0]
+    table.innerHTML = ""
     let activePet = getActivePet()
-    table.classList.add("activetable")
 
     for (let i = 0; i < 4; i++) { //4 rows
         let row = table.insertRow()
-        row.classList.add("activetablerow")
         for(let j = 0; j < 5; j++) { //5 per row
             let index = i*5 + j
             if(index < petList.length) {
@@ -115,76 +125,58 @@ function createTable() {
 
                 let cell = row.insertCell()
                 cell.setAttribute("name", name)
-                if(activePet == name) {
-                    cell.classList.add("activepetcell")
-                    cell.innerHTML = `<img src=${img} width="150" height="150" alt=${name}><br>(already active)`
-                }
+                //name = "test"
+                let d1 = document.createElement("div"), d2 = document.createElement("div")
+                d1.innerHTML = `<img src=${img} width="150" height="150" alt=${name}>`
+                d1.style.width = "150px !important";
+                d1.style.height = "150px !important";
+                d2.innerHTML = name
+                cell.appendChild(d1)
+                cell.appendChild(d2)
+                if(activePet == name) cell.setAttribute("active", "")
                 else {
-                    cell.innerHTML = `<img src=${img} width="150" height="150" alt=${name}><br>${name}`
-                    cell.classList.add("activetablecell")
                     cell.addEventListener("click", (event)=>{event.stopPropagation(); changeActivePet(name);})
                     cell.style.cursor = "pointer"
                 }
             }
         }
     }
-    return table
 }
-
 
 //=====================
 //element functionality
 //=====================
 
 function openMenu() {
-    if(GM_getValue("petlist") && GM_getValue("userref")) {
-        $("#select-active-pet-menu")[0].style.display = "flex"
-        if(isBeta) {
-            $("#container__2020")[0].style.display = "none"
-            $("#navprofiledropdown__2020")[0].style.display = "none"
-            $("#navdropdownshade__2020")[0].style.display = "none"
-            $(".navsub-left__2020")[0].style.display = "none"
-            $(".navsub-right__2020")[0].style.display = "none"
-        }
-        else {
-            $("#content")[0].style.display = "none"
-        }
-        //closes menu if click happens
-
-        setTimeout(()=>{document.body.addEventListener("click", exitClick, false)}, 50)
-    }
-    else {
-        if(window.confirm("ERROR: You have to visit the homepage before you are able to use this script!\n\nVisit the homepage now?")) window.location.href = "https://www.neopets.com/home/"
-        console.log("[APS] Data missing, visit https://www.neopets.com/home/ to populate.")
-    }
+    $("#select-active-pet-menu").css("display","flex") //makes menu visible
+    setTimeout(()=>{document.body.addEventListener("click", exitClick, false)}, 50) //adds the exit click a short delay after menu is created
 }
 
 function exitClick(event) {
-    if(!document.querySelector(".activetable").contains(event.target)) {
+    event.stopPropagation()
+    if(!(document.querySelector(".activetableheader").contains(event.target) || document.querySelector(".activetable").contains(event.target))) {
         $("#select-active-pet-menu")[0].style.display = "none"
-        if(isBeta) {
-            $("#container__2020")[0].style.display = "block"
-            $(".navsub-left__2020")[0].style.display = "block"
-            $(".navsub-right__2020")[0].style.display = "block"
-        }
-        else {
-            $("#content")[0].style.display = "block"
-        }
         document.body.removeEventListener("click", exitClick, false)
     }
 }
 
 function changeActivePet(name) {
+    //waits for data if the flag is on
+    if(GM_getValue("waitfordata", false)) {
+        console.log("[APS] Waiting for data to finish updating...")
+        GM_addValueChangeListener("waitfordata", function(key,oldValue,newValue,remote) {
+            sendActivePetReq(name)
+        })
+    }
+    //otherwise do it now
+    else {
+        sendActivePetReq(name)
+    }
+}
+
+function sendActivePetReq(name) {
     //hides menu once a selection is made
     $("#select-active-pet-menu")[0].style.display = "none"
-    if(isBeta) {
-        $("#container__2020")[0].style.display = "block"
-        $(".navsub-left__2020")[0].style.display = "block"
-        $(".navsub-right__2020")[0].style.display = "block"
-    }
-    else {
-        $("#content")[0].style.display = "block"
-    }
     let ref = GM_getValue("userref")
     $.ajax({
         url: "/np-templates/ajax/changepet.php",
@@ -207,17 +199,17 @@ function changeActivePet(name) {
 //========
 
 function getUsername() {
-    if(isBeta) return $("#navprofiledropdown__2020 > div:nth-child(3) > a")[0].innerHTML
-    else return $("#header > table > tbody > tr:nth-child(1) > td.user.medText > a:nth-child(1)")[0].innerHTML
+    if(isBeta) return $("#navprofiledropdown__2020 > div:nth-child(3) a.text-muted")[0].innerHTML
+    else return $("#header > table > tbody > tr:nth-child(1) > td.user.medText a[href]")[0].innerHTML
 }
 
-function getUserRef() {
-    let src = document.documentElement.outerHTML.split(/[\r\n]+/).find( l => l.includes('"_ref_ck":'))
+function getUserRef(doc) {
+    let src = doc.documentElement.outerHTML.split(/[\r\n]+/).find( l => l.includes('"_ref_ck":'))
     return src.split(":")[1].replaceAll(/[^a-zA-Z\d]/g, "").trim()
 }
 
-function getPets() {
-    let elements = Array.from($(".hp-carousel-pet"))
+function getPets(doc) {
+    let elements = Array.from(doc.querySelectorAll(".hp-carousel-pet"))
     let petNames = elements.map(e => e.getAttribute("data-name"))
     petNames = petNames.filter((n, index) => petNames.indexOf(n) == index)
     let petInfo = {}
@@ -235,7 +227,12 @@ function getPets() {
         petInfo[name] = {name:name, species:species, color:color, img:img, gender:gender, hp:hp, level:level, hunger:hunger, mood:mood, p2:p2}
     }
     if(petInfo == {}) return null
-    else return petInfo
+    //pet list should be in alphabetical order
+    let sorted = Object.keys(petInfo).sort().reduce((obj, key) => {
+        obj[key] = petInfo[key]
+        return obj
+    }, {})
+    return sorted
 }
 
 function getPetpet(e) {
@@ -258,16 +255,6 @@ function getActivePet() {
     else return $("#content > table > tbody > tr > td.sidebar > div:nth-child(1) > table > tbody > tr:nth-child(1) > td > a > b")[0].innerHTML
 }
 
-function getThemeColor() {
-    if(isBeta) return $(".nav-profile-dropdown__2020").css("background-color")
-    else return $("#content > table > tbody > tr > td.sidebar > div:nth-child(1) > table > tbody > tr:nth-child(1) > td").css("background-color")
-}
-
-function getTextColor() {
-    if(isBeta) return $(".nav-profile-dropdown-text").css("color")
-    else return "black"
-}
-
 function getButtonLocation() {
     if(isBeta) var e = $("#navprofiledropdown__2020 > div:nth-child(4)")
     else e = $(".activePet")
@@ -281,69 +268,171 @@ function getButtonLocation() {
 //========
 
 function addCSS() {
-    document.head.appendChild(document.createElement("style")).innerHTML = `
-        .activepetmenu {
-            background-color: #8a8a8a;
-            width: 100%;
-            height: 100%;
-            flex-direction: column;
-        }
+    if(isBeta) {
+        var theme = $(".nav-profile-dropdown__2020").css("background-color")
+        var button = $(".nav-profile-dropdown-text").css("color")
+        var text = $(".nav-profile-dropdown-text a.text-muted").css("color")
+    }
+    else {
+        theme = $("#content > table > tbody > tr > td.sidebar > div:nth-child(1) > table > tbody > tr:nth-child(1) > td").css("background-color")
+        button = "gray";
+        text = "black";
+    }
+
+    if(isBeta) {
+        document.head.appendChild(document.createElement("style")).innerHTML = `
         .openmenubutton {
-            color: blue !important;
-            background-color: #a3a3a3;
-            border: 2px solid #4f4f4f;
+            border: 2px solid ${text};
+            color: ${text} !important;
+            background-color: ${button};
             cursor: pointer;
-            font-size: 8pt;
+            font-size: 11pt;
             padding: 1px 0px 1px;
             width: 90%;
             height: 100%;
             display: block;
             font-weight: normal;
             text-align: center;
+            transition-duration: 0.3s;
+            border-radius: 4px;
+            box-shadow: 0 5px rgba(0,0,0,0.5);
+        }
+        .openmenubutton:hover {
+            border: 2px solid ${text.slice(0,-1)+", 0.85)"};
+            color: ${text.slice(0,-1)+", 0.85)"} !important;
+            background-color: ${button.slice(0,-1)+", 0.7)"};
+        }
+        .openmenubutton:active {
+            border: 2px solid ${theme};
+            background-color: ${button.slice(0,-1)+", 0.5)"};
+            box-shadow: 0 2px rgba(0,0,0,0.5);
+            transform: translateY(2px);
+        }
+        `
+    }
+    else {
+        document.head.appendChild(document.createElement("style")).innerHTML = `
+        .openmenubutton {
+            margin-top: 3px;
+            border: 1px solid black;
+            background-color: #D3D3D3;
+            cursor: pointer;
+            font-size: 8pt;
+            padding: 1px 0px 1px;
+            width: 148px;
+            height: 100%;
+            display: block;
+            font-weight: normal;
+            text-align: center;
+        }
+        .openmenubutton:hover {
+            background-color: #C0C0C0;
+        }
+        .openmenubutton:active {
+            background-color: #808080;
+        }
+        `
+    }
+
+    document.head.appendChild(document.createElement("style")).innerHTML = `
+        #select-active-pet-menu {
+            position: fixed;
+            z-index: 100000;
+            background-color: #8a8a8a;
+            flex-direction: column;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+        #select-active-pet-menu::before {
+            width: 100vw;
+            height: 100vh;
+            opacity: 0.7;
+            background-color: black;
+            z-index: -1;
+            content: "";
+            position: fixed;
+            transform: translate(-50%, -50%);
+            left: 50%;
+            top: 50%;
         }
         .activetableheader {
             text-align: center;
             font-size: 16pt;
             cursor: default;
-            padding: 4px;
+            background-color: ${theme};
+            color: ${text};
+            padding-top: 8px;
+            padding-bottom: 8px;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
         }
         .activetable {
-            display: table;
-            margin: 0 8%;
             padding: 20px 20px;
             text-align: center;
-            width: auto;
-            height: auto;
             background-color: #FFFFFF;
+            border-spacing: 5px;
         }
-        .activetablerow {
-            display: table-row;
-            align-items: center;
-            justify-content: center;
-        }
-        .activetablecell {
-            display: table-cell;
-            padding-top: 6px;
+        .activetable td {
             font-weight: bold;
             text-align: center;
             background-color: #e6e6e6;
+            border-radius: 8px;
+            padding-top: 10px;
+            padding-bottom: 5px;
+            position: relative;
         }
-        .activetablecell:hover {
+        .activetable td img {
+            margin: 0;
+            padding: 0;
+            width: 150px !important;
+            pointer-events: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        .activetable td div {
+            margin: 0;
+            padding: 0;
+            width: 170px !important;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        .activetable td:hover {
             background-color: #c9e3b3;
         }
-        .activetablecell:hover img {
-            opacity: 0.95;
+        .activetable td:active {
+            background-color: #a2cf7c;
+        }
+        .activetable td:hover img {
+            opacity: 0.85;
         }
 
-        .activepetcell {
-            padding-top: 6px;
-            display: table-cell;
-            text-align: center;
+        .activetable td[active] {
             cursor: not-allowed;
             background-color: #8f8f8f;
             color: #4a4a4a;
         }
-        .activepetcell img {
+        .activetable td[active]::after {
+            opacity: 0.6;
+            font-size: 16pt;
+            text-align: center;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            cursor: not-allowed;
+            color: black;
+            content: "(already active)";
+            position: absolute;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transform: rotate(25deg)
+        }
+        .activetable td[active] img {
             opacity: 0.3;
         }
     `
